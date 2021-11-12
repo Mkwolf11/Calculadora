5c673f31e8cb33f5894a9491d6d33da408cb5a52
Mkwolf11 <mcf94@iesornia.com>
2021-11-12 16:56:42 +0100

    Mejora del codigo a la hora de crear la calculadora

--- src/main/java/com/mycompany/calculadora/Calculadora.java
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -11,68 +11,77 @@ import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
 /**
  *
  * @author mcabe
  */
-public class Calculadora extends Frame {
+public class Calculadora extends Frame implements ActionListener {
+    // string con los numeros que despues mostrare en un panel
+    public String [] Numeros = {"7","8","9","4","5","6","1","2","3",".","0","Ce"};
+    // String con las operaciones que despues mostrare en un panel
+    public String [] Operaciones = {"     +     ", "     -     ","     *     "     ,     "     /     ","     =     "}; 
+
     
     int resultado = 0;
     int acumulador = 0;
+    TextField texto;
+    
     public Calculadora(){
+       
         
         //panel de texto
-        Panel texto= new Panel();
-        texto.setLayout (new GridLayout (1,0));
-        texto.add(new Label());
+        Panel ptexto = new Panel();
+        texto = new TextField(200);
+        ptexto.setLayout (new GridLayout (1,0));
+        ptexto.add(new Label());
+        setSize(400,200);
 
         //panel operaciones
-        Panel operaciones= new Panel();
-        operaciones.setLayout (new GridLayout (0,1));
-        operaciones.add(new Button("+"));
-        operaciones.add(new Button("-"));
-        operaciones.add(new Button("*"));
-        operaciones.add(new Button("/"));
-        operaciones.add(new Button("="));
+        Panel pnlOperaciones= new Panel();
+        pnlOperaciones.setLayout (new GridLayout (0,1));
+         for (int k=0;k<Operaciones.length;k++) {
+            Button btnOperacion = new Button(Operaciones[k]);
+            btnOperacion.addActionListener(this);        
+            pnlOperaciones.add(btnOperacion);
+        }
    
         
-        //panel numeros
-        Panel numeros = new Panel();
-        numeros.setLayout (new GridLayout (4,3));
-
-        numeros.add(new Button("7"));
-        numeros.add(new Button("8"));
-        numeros.add(new Button("9"));
-        numeros.add(new Button("4"));
-        numeros.add(new Button("5"));
-        numeros.add(new Button("6"));
-        numeros.add (new Button("1"));
-        numeros.add(new Button("2"));
-        numeros.add(new Button("3"));
-        numeros.add(new Button("0"));
-        numeros.add(new Button("CE"));
+        //panel pnlNumeros
+        Panel pnlNumeros = new Panel();
+        pnlNumeros.setLayout (new GridLayout (4,3));
+        for (int k=0;k<Numeros.length;k++) {
+            Button btnNumero = new Button(Numeros[k]);
+            btnNumero.addActionListener(this);        
+            pnlNumeros.add(btnNumero);
+        }  
         
         
         //panel principal
         Panel principal = new Panel();
         principal.setLayout (new BorderLayout());
-        principal.add(numeros,BorderLayout.CENTER);
+        principal.add(pnlNumeros,BorderLayout.CENTER);
         principal.add(texto,BorderLayout.NORTH);
-        principal.add(operaciones,BorderLayout.EAST);
+        principal.add(pnlOperaciones,BorderLayout.EAST);
         setSize(400,500);
-        setTitle("Ventana");
+        setTitle("Ventana Calculadora Marcos.cabfro");
         this.add(principal);
         setVisible(true);
+        setLocationRelativeTo(null);
 
-    }
-    public void windowClosing(WindowEvent e){
-        System.exit(0);
     }
     
     public static void main (String [] args){
         Calculadora c = new Calculadora();
         c.setVisible(true);
     } 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
